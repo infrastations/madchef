4d6a0b54279482734544f7c96056df12cbecb3cc
Md. Rakib Trofder <mdrakibtrofder@gmail.com>
2026-01-21 18:42:58 +0600

    feature: configuration for deploying at github

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/madchef">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
